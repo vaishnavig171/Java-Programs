cc41a31c2730c04d3104291719c0a3cdbb78d23c
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-07 18:54:57 +0530

    Data Types in Java

---
+++ DataTypesEx.java
@@ -0,0 +1,24 @@
+class DataTypesEx
+{
+	public static void main(String args[]){
+		int myNum = 4;
+		float myFloat = 5.99f;
+		char myChar = 'V';
+		Boolean myBool = true;
+		double myDnum = 19.99d;
+		byte myNo = 100;
+		short mySno = 5000;
+		long myLno = 80000000000L;
+		String myText = "Hello";
+		
+		System.out.println("Integer: " +myNum);
+		System.out.println("Float: " +myFloat);
+		System.out.println("Character: " +myChar);
+		System.out.println("Boolean: " +myBool);
+		System.out.println("Double: " +myDnum);
+		System.out.println("Byte: " +myNo);
+		System.out.println("Short: " +mySno);
+		System.out.println("Long: " +myLno);
+		System.out.println("String: " +myText);
+	}
+}
