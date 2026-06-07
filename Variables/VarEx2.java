2efc58f96fee010f43ad633e02744afb95effd26
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-07 21:26:21 +0530

    Variable example 2

---
+++ Variables/VarEx2.java
@@ -0,0 +1,11 @@
+/* Variable Example 2
+Combining two strings */
+public class VarEx2 {
+    public static void main (String args[]){
+        String firstName = "Andrew";
+        String lastName = "Garfield";
+        String fullName = firstName + lastName;
+
+        System.out.println("My name is " +fullName);
+    }
+}
