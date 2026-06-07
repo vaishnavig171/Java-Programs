c507c8df651580a00611de6d593876ec983db31f
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-07 21:03:28 +0530

    Variable Example1

---
+++ Variables/Var1.java
@@ -0,0 +1,9 @@
+//Variable Example1
+//Creation of a variable called name (String) and assigning a value "Vaishnavi"
+public class Var1 {
+    public static void main(String args[]){
+        String name = "Vaishnavi";
+
+        System.out.println("My name is " +name);
+    }
+}
