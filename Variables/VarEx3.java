cb74d110753bf37d349ff5944d2704352eefd81f
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-07 21:31:48 +0530

    Variables Example 3

---
+++ Variables/VarEx3.java
@@ -0,0 +1,12 @@
+/* Variables Example 3
+Adding 3 numbers using + operator 
+For numeric values, + operator works as a mathematical operator */
+public class VarEx3 {
+    public static void main(String args[]){
+        int a = 5;
+        int b = 5;
+        int c = 10;
+        int d = a + b + c;
+        System.out.println("Addition is " +d);
+    }
+}
