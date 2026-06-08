a392ca12d193a559a6373c9ff32896e705af2c06
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-08 20:27:17 +0530

    Standard Default Values

---
+++ DefaultValues.java
@@ -0,0 +1,20 @@
+public class DefaultValues {
+    int i;
+    float f;
+    double d;
+    long l;
+    boolean b;
+    char c;
+    String s;
+    public static void main(String args[]){
+        DefaultValues df = new DefaultValues();
+
+        System.out.println(df.i);
+        System.out.println(df.f);
+        System.out.println(df.d);
+        System.out.println(df.l);
+        System.out.println(df.b);
+        System.out.println(df.c);
+        System.out.println(df.s);
+    }
+}
