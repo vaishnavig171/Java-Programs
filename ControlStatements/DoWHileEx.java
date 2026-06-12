4095cf1082f50d6476e1b9acdb916cd044197039
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-12 19:52:16 +0530

    Do while loop example

---
+++ ControlStatements/DoWHileEx.java
@@ -0,0 +1,11 @@
+public class DoWHileEx {
+    public static void main(String args[])
+    {
+        int i = 1;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
