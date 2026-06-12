32e27350b5d441222497e7c78b59a2b8be9a73b2
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-12 20:45:57 +0530

    Method Example

---
+++ OopsConcept/MethodEx.java
@@ -0,0 +1,9 @@
+class MethodEx {
+    int id;     //instance variable
+    String name;
+    public static void main(String args[]){
+        MethodEx s1 = new MethodEx();   //creating an object
+        System.out.println(s1.id);  //accessing member through reference variable
+        System.out.println(s1.name);
+    }    
+}
