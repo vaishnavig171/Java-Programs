3b8fc26650db89f4163585b9faa2a33b060150d7
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-16 23:44:53 +0530

    Delete OopsConcept/MethodEx.java

--- OopsConcept/MethodEx.java
+++
@@ -1,9 +0,0 @@
-class MethodEx {
-    int id;     //instance variable
-    String name;
-    public static void main(String args[]){
-        MethodEx s1 = new MethodEx();   //creating an object
-        System.out.println(s1.id);  //accessing member through reference variable
-        System.out.println(s1.name);
-    }    
-}
