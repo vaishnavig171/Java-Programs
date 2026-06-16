2f838bf51463050552c1aca9ed4aca72d1bb5b2c
Vaishnavi Gite <gitevaishnavi25@gmail.com>
2026-06-16 23:53:29 +0530

    Method example1

---
+++ OopsConcept/MethodExample.java
@@ -0,0 +1,10 @@
+class MethodExample {
+
+    static void display() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String args[]) {
+        display(); // Method call
+    }
+}
